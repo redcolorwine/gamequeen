41847c8b597742f3f86c6ddd128fecb601b66e3b
LobetzSerg <lobet1501@gmail.com>
2023-08-16 16:52:48 +0700

    commit 7

--- src/pages/GameAbout/GameAbout.jsx
+++ src/pages/GameAbout/GameAbout.jsx
@@ -14,8 +14,10 @@ import web from './../../media/icons/platforms/web.png';
 import DOMPurify from 'dompurify';
 import defaultImg from './../../media/images/gameu.jpg';
 import { AiOutlineClose, AiOutlineHeart, AiOutlineMenu } from "react-icons/ai";
-import { FcLike } from "react-icons/fc";
+import { FcLike, FcCancel, FcPrevious, FcNext } from "react-icons/fc";
+import { BsFillCaretLeftFill, BsFillCaretRightFill, BsXCircleFill } from "react-icons/bs";
 const GameAbout = (props) => {
+
     useEffect(() => {
         if (localStorage.getItem('userId')) {
             props.getWishThunk(localStorage.getItem('userId'))
@@ -37,8 +39,8 @@ const GameAbout = (props) => {
         'Nintendo': nintendo,
         'Web': web
     }
-    const [curScreen, setCurScreen] = useState(0)
-    
+    const [curScreen, setCurScreen] = useState(0);
+    const [openImg, setOpenImg] = useState(false);
     const { id } = useParams();
 
     const addWish = (e) => {
@@ -60,47 +62,91 @@ const GameAbout = (props) => {
         }
     }
 
-
+    const clickLeftArrow = () => {
+        let lengthImages;
+        if (props.gameScreenshots.data?.results.length) {
+            lengthImages = props.gameScreenshots.data.results.length;
+            if (curScreen >= 1) {
+                setCurScreen(curScreen - 1)
+            } else if (curScreen == 0) {
+                setCurScreen(lengthImages - 1)
+            }
+        }
+    }
+    const clickRightArrow = () => {
+        let lengthImages;
+        if (props.gameScreenshots.data?.results.length) {
+            lengthImages = props.gameScreenshots.data.results.length;
+            if (curScreen < lengthImages - 1) {
+                setCurScreen(curScreen + 1)
+            } else if (curScreen == lengthImages - 1) {
+                setCurScreen(0)
+            }
+        }
+    }
+   
     if (props.isGameInfoLoading && !props.wishData.id) {
-
         return (
             <><Preloader /></>
         )
-
     }
+
     else {
-        console.log(props.wishData.find(e => e.id == id))
+        console.log(props.gameScreenshots.data?.results.length)
         return (
             <div className={cmedia.gameAbout}>
+
+                {openImg &&
+                    <div className={cmedia.openImg}>
+                        {props.gameScreenshots.data?.results ? <img src={props.gameScreenshots.data.results[curScreen].image} /> : <img src={defaultImg} />}
+                        <div className={cmedia.closeBut}><BsXCircleFill color='white' fill='white' size={45} onClick={() => { setOpenImg(false) }} /></div>
+                        <div className={cmedia.arrows}>
+                            <div className={cmedia.arrowButtons}>
+                                <div className={cmedia.arrow}>
+                                    <BsFillCaretLeftFill size={65} color='white' fill='white' onClick={() => clickLeftArrow()} />
+                                </div>
+                                <div className={cmedia.arrow}>
+                                    <BsFillCaretRightFill size={65} color='white' fill='white' onClick={() => clickRightArrow()} />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>}
+
                 <div className={cmedia.head}>
                     <h1>{props.gameInfo.data.name}</h1>
-                   
-
                 </div>
+
                 <div className={cmedia.firstBlock}>
 
                     <div className={cmedia.shortDesc}>
+
                         <h2>Short description</h2>
+
                         <div className={cmedia.platforms}>
-                            
                             {props.gameInfo.data.parent_platforms.map(pl => {
                                 let plat = pl.platform.name;
                                 return (<><img src={platformImgs[plat]} /></>)
                             })}
                         </div>
+
                         {props.wishData.find(e => e.id == id) ? <div className={cmedia.wishButton} onClick={(e) => deleteWish(e)}><FcLike size={25} /> Already in your wishlist </div>
-                        : <div className={cmedia.wishButton} onClick={(e) => addWish(e)}><AiOutlineHeart color='red' size={25} />  WISH</div>
-                    }
+                            : <div className={cmedia.wishButton} onClick={(e) => addWish(e)}><AiOutlineHeart color='red' size={25} />  WISH</div>
+                        }
+
                         <p><span>Added: </span>{props.gameInfo.data.added} </p>
+
                         <div className={cmedia.developers}>
                             <p><span>Developers:</span> {props.gameInfo.data?.developers ? props.gameInfo.data.developers.map(dev => {
                                 return (<> {dev.name} </>)
                             }) : ''}</p>
+
                             <div className={cmedia.devImgs}>
                                 {props.gameInfo.data?.developers ? props.gameInfo.data.developers.map(dev => {
                                     return (<> {dev?.image_background ? <img src={dev.image_background} /> : ''} </>)
                                 }) : ''}
                             </div>
+
                         </div>
                         <div className={cmedia.genres}>
                             <p><span>Genres: </span>
@@ -115,7 +161,7 @@ const GameAbout = (props) => {
                         <div className={cmedia.metacritic}>
                             <p><span>Metacritic: </span><a href={props.gameInfo.data?.metacritic_url ? props.gameInfo.data.metacritic_url : '#'}>{props.gameInfo.data?.metacritic ? props.gameInfo.data.metacritic : ''} </a></p>
                         </div>
-                       
+
                         <div className={cmedia.rating}>
                             <p><span>Rating: </span>{props.gameInfo.data?.rating ? props.gameInfo.data.rating : ''}</p>
                         </div>
@@ -136,7 +182,7 @@ const GameAbout = (props) => {
                     </div>
                     <div className={cmedia.screens}>
                         <div className={cmedia.mainImg}>
-                            {props.gameScreenshots.data?.results ? <img src={props.gameScreenshots.data.results[curScreen].image} /> : <img src={defaultImg} />}
+                            {props.gameScreenshots.data?.results ? <img onClick={() => setOpenImg(true)} src={props.gameScreenshots.data.results[curScreen].image} /> : <img src={defaultImg} />}
                         </div>
                         <div className={cmedia.otherImgs}>
                             {props.gameScreenshots.data?.results ? props.gameScreenshots.data.results.map((item, index) => {
